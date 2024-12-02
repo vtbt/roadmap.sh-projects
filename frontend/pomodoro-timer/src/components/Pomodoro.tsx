7adc47f6ae040175a9ad2d8141151d7ed4775e65
Vu Thi Bich Thu <vuthibichthu.dev@gmail.com>
2024-12-02 20:58:26 +0700

    WIP - Create a pomodoro tracker application for productivity

--- frontend/pomodoro-timer/src/components/Pomodoro.tsx
+++ frontend/pomodoro-timer/src/components/Pomodoro.tsx
@@ -1,16 +1,78 @@
+import { FC, useEffect, useState } from 'react';
 import styles from './Pomodoro.module.css';
 
-const Pomodoro = () => {
+const Pomodoro: FC = () => {
+  const [timerValue, setTimerValue] = useState(25);
+  const [timeLeft, setTimeLeft] = useState(timerValue * 60);
+  const [isRunning, setIsRunning] = useState(false);
+
+  //   get values from settings
+
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval> | null = null;
+
+    if (isRunning && timeLeft > 0) {
+      interval = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeLeft === 0) {
+      handleTimerComplete();
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isRunning, timeLeft]);
+
+  const handleTimerComplete = () => {
+    setIsRunning(false);
+
+    // Determine next mode
+    // switch (mode) {
+    //   case 'work':
+    //     setCompletedCycles(prev => prev + 1);
+    //     if (completedCycles + 1 === settings.cyclesBeforeLongBreak) {
+    //       setMode('longBreak');
+    //       setTimeLeft(settings.longBreakDuration * 60);
+    //     } else {
+    //       setMode('break');
+    //       setTimeLeft(settings.shortBreakDuration * 60);
+    //     }
+    //     break;
+    //   case 'break':
+    //     setMode('work');
+    //     setTimeLeft(settings.workDuration * 60);
+    //     break;
+    //   case 'longBreak':
+    //     setMode('work');
+    //     setTimeLeft(settings.workDuration * 60);
+    //     setCompletedCycles(0);
+    //     break;
+    // }
+  };
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds
+      .toString()
+      .padStart(2, '0')}`;
+  };
+
   return (
     <div className={styles.pomodoro}>
       <div className={styles.wrapper}>
         <div className={styles.buttonGroup}>
-          <button>Pomodoro</button>
-          <button>Short break</button>
-          <button>Long break</button>
+          <button onClick={() => setTimerValue(25)}>Pomodoro</button>
+          <button onClick={() => setTimerValue(5)}>Short break</button>
+          <button onClick={() => setTimerValue(15)}>Long break</button>
         </div>
-        <p className={styles.timer}>25:00</p>
-        <button>Start</button>
+        <p className={styles.timer}> {formatTime(timeLeft)}</p>
+        {isRunning ? (
+          <button onClick={() => setIsRunning(false)}>Stop</button>
+        ) : (
+          <button onClick={() => setIsRunning(true)}>Start</button>
+        )}
       </div>
     </div>
   );
--- frontend/pomodoro-timer/src/components/Settings.tsx
+++ frontend/pomodoro-timer/src/components/Settings.tsx
@@ -1,6 +1,7 @@
+import { FC } from 'react';
 import styles from './Settings.module.css';
 
-const Settings = () => {
+const Settings: FC = () => {
   return (
     <div className={styles.settings}>
       <div className={styles.wrapper}>
